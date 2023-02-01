01df5c1d03ec51b660f39d494d344f22d851bc9d
Toan Nguyen <nguyentritoanst@gmail.com>
2023-02-02 05:51:01 +0700

    Job Post Submit

--- src/screens/jobPosts/JobPostCreateScreen.tsx
+++ src/screens/jobPosts/JobPostCreateScreen.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import CreateForm from '../../components/forms/CreateForm'
+import React from 'react';
+import Toast from 'react-native-root-toast';
+import CreateForm from '../../components/forms/CreateForm';
+import dataProvider from '../../dataProvider';
 
 const JobPostCreateScreen = () => {
+  const { mutateAsync } = useMutation({
+    mutationFn: (data) => dataProvider.create('jobposts', { data }),
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    mutateAsync(data)
+      .then(() => queryClient.invalidateQueries({ queryKey: ['login'] }))
+      .catch(() => Toast.show('Create failed', { position: Toast.positions.CENTER }));
+  };
   return (
-    <CreateForm />
+    <CreateForm onSubmit={onSubmit} />
   )
 }
 
